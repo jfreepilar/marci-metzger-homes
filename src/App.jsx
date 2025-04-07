d509d883cf8ec37e0f0d9b7c1664f883f89ce177
Joeffrey <tipsjsp@gmail.com>
2025-04-08 01:24:40 +0800

    feat: Add favicon to improve the desidn of the web page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 
-
-
 function App() {
 
   return (
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/marci-metzger-homes/',
   plugins: [
     react(),
     tailwindcss(),
